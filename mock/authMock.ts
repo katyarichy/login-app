9db8902be2ff76e3463276c1bc1b74fc0ff6de33
Kateryna Marchenko <katya.richy@gmail.com>
2024-05-31 19:51:32 +0200

    add mock data

---
+++ mock/authMock.ts
@@ -0,0 +1,11 @@
+export const authMock = {
+  users: [
+    {
+      username: 'admin',
+      password: '1234',
+      token: 'valid-token',
+      role: 'Administrator',
+    },
+    { username: 'user', password: '5678', token: 'valid-token', role: 'User' },
+  ],
+};
