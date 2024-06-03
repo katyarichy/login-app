75ecc70af74f88a195eab6985e6298dd5c5a2996
Kateryna Marchenko <katya.richy@gmail.com>
2024-06-03 14:37:43 +0200

    integrate ag grid for data display

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,9 +1,31 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 const Dashboard = () => {
   const [data, setData] = useState<any[]>([]);
+  const columnDefs = [
+    {
+      headerName: 'Catalog Description',
+      field: 'CatalogDescription',
+      flex: 1,
+      minWidth: 150,
+    },
+    { headerName: 'Item Code', field: 'ItemCode', flex: 1, minWidth: 100 },
+    { headerName: 'Sales Price', field: 'SalesPrice', flex: 1, minWidth: 100 },
+    {
+      headerName: 'Stock',
+      field: 'LastAvailableStock',
+      flex: 1,
+      minWidth: 100,
+    },
+  ];
+
+  const [paginationPageSize] = useState(30);
+  const [paginationPageSizeOptions] = useState([30, 60, 90]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,13 +46,19 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item.CatalogDescription}</li>
-        ))}
-      </ul>
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <div
+        className='ag-theme-alpine'
+        style={{ height: '100%', width: '100%' }}
+      >
+        <AgGridReact
+          rowData={data}
+          columnDefs={columnDefs}
+          pagination={true}
+          paginationPageSize={paginationPageSize}
+          paginationPageSizeSelector={paginationPageSizeOptions}
+        />
+      </div>
     </div>
   );
 };
